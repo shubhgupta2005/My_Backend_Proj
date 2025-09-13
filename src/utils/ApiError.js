683264298a56ce59f9ae9cbfba6ecc1b81b55408
Shubh Gupta <gshubh2005@gmail.com>
2025-09-13 16:23:32 +0530

    project setup files part 2

---
+++ src/utils/ApiError.js
@@ -0,0 +1,26 @@
+
+
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message="Something Went Wrong",
+        errors=[],
+        statck=""
+    ){
+        super(message)
+        this,statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success=false
+        this.errors=errors
+        this.statck=statck
+
+        if(statck){
+            this.statck=statck
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
